85d48c9e0a41b30f93c09dbd0ca49b1498a6d4ba
cpayment <cpayment12@gmail.com>
2024-02-01 19:44:11 -0600

    Added 'View All Employees', 'View All Roles', and 'View All Departments' functionality and notes

--- index.js
+++ index.js
@@ -21,8 +21,46 @@ function displayMenu() {
         ])
         .then((response) => {
             switch(response.userInput) {
+
+                // Select "View All Employees" to be presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
+                case 'View All Employees':
+                    const employees = [
+                        { id: 1, first_name: 'Cody', last_name: 'Payment', title: 'Sales Lead', department: 'Sales', salary: '100000', manager: 'null'},
+                    ];
+
+                    const employeesTable = new Table({
+                        head: ['ID', 'First Name', 'Last Name', 'Title', 'Department', 'Salary', 'Manager'],
+                    });
+
+                    employees.forEach((employee) => {
+                        employeesTable.push([employee.id, employee.first_name, employee.last_name, employee.title, employee.department, employee.salary, employee.manager]);
+                    });
+
+                    console.log(employeesTable.toString());
+                    break;
+
                 // Select "View All Roles" to be presented with the job title, role id, the department that role belongs to, and the salary for that role
                 case 'View All Roles':
+                    const roles = [
+                        { id: 1, title: 'Sales Lead', department: 'Sales', salary: '100000' },
+                        { id: 2, title: 'Salesperson', department: 'Sales', salary: '80000' },
+                        { id: 3, title: 'Lead Engineer', department: 'Engineering', salary: '150000' },
+                        { id: 4, title: 'Software Engineer', department: 'Engineering', salary: '120000' },
+                        { id: 5, title: 'Account Manager', department: 'Finance', salary: '160000' },
+                        { id: 6, title: 'Accountant', department: 'Finance', salary: '125000' },
+                        { id: 7, title: 'Legal Team Lead', department: 'Legal', salary: '250000' },
+                        { id: 8, title: 'Lawyer', department: 'Legal', salary: '190000' },
+                    ];
+
+                    const rolesTable = new Table({
+                        head: ['ID', 'Title', 'Department', 'Salary'],
+                    });
+
+                    roles.forEach((role) => {
+                        rolesTable.push([role.id, role.title, role.department, role.salary]);
+                    });
+
+                    console.log(rolesTable.toString());
                     break;
 
                 // Select "View All Departments" to be presented with a formatted table showing department names and department ids
@@ -34,22 +72,21 @@ function displayMenu() {
                         { id: 4, name: 'Legal' },
                     ];
 
-                    const table = new Table({
+                    const departmentsTable = new Table({
                         head: ['ID', 'Department Name'],
                     });
 
                     departments.forEach((department) => {
-                        table.push([department.id, department.name]);
+                        departmentsTable.push([department.id, department.name]);
                     });
 
-                    console.log(table.toString());
+                    console.log(departmentsTable.toString());
                     break;
 
                 // Select "Quit" to exit the program
                 case 'Quit':
                     console.log('Exiting the Program...');
                     process.exit();
-                    break;
 
                 // Use default response if action is invalid
                 default:
